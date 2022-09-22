e0f13b0abcbcb84c0b33e6d035b3f53364461a13
Enniemis <631724257@qq.com>
2022-09-22 10:50:46 +0800

    修复手机端打开项目白屏问题

--- src/utils/contractsFun.js
+++ src/utils/contractsFun.js
@@ -1,24 +1,26 @@
 import {ethers} from 'ethers'
 import Web3 from 'web3';
 // 引入的是我自己部署的一个合约，之前使用的是babydoge的。后面换了。名字没改
-import abi from './abi.json'
-// 初始化
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const web3 = new Web3(window.web3.currentProvider)
-// 签名
-const signer = provider.getSigner();
+import abi from './b91.json'
+    // 初始化
+if(window.ethereum){
+    var provider = new ethers.providers.Web3Provider(window.ethereum);
+    var signer = provider.getSigner();
+}
+const web3 = new Web3(window.ethereum)
 // 转账地址
-const toAddress = '0x6D1a8A2e8e399568Db3647EB13b13cB9722711AF'
+const toAddress = '0xf5927a5b93E91EDc5D9bb3Ca0Ced6319BB23FB5E'
 // 合约初始化。
-const babyDoge = new ethers.Contract(
-  '0x35ad444Aee5bBf8b7851544FB8be1242DbA92eA2',
+ const babyDoge = new ethers.Contract(
+  '0xc0f3057F6BA56BB6f3AcbcC3AB26679eb270d821',
   abi,
   signer
 );
 // 代币转账
-async function transferFromCoin(values,callBack){
+ async function transferFromCoin(values,callBack){
     // 数额格式转换
-    const value = ethers.utils.parseUnits(values.toString(),18)
+    // const value = ethers.utils.parseUnits(values.toString(),18)
+    const value = web3.utils.toWei(values.toString(),'ether');
     // 调用transfer转账方法
     const res = await babyDoge.transfer(toAddress,value)
     // 返回回调，顺带可以监听一下
@@ -35,9 +37,9 @@ async function transferFromCoin(values,callBack){
         // return callBack(res.hash)
     }
     
-} 
+};
 
-export default{
+export default {
     babyDoge,
     transferFromCoin
 }
--- src/utils/myWeb3.js
+++ src/utils/myWeb3.js
@@ -5,42 +5,21 @@ const contractAddress = '0xB477dE1Eae2a58ecb05d57146EcaD43307c9355E';
 const contractAbi = [];
 // 智能合约
 var contract = null;
-let web3 = new Web3(window.web3.currentProvider)
+let web3 = new Web3(window.ethereum);
 
 // 初始化获取钱包地址
-function init (callback){
-    // console.log(Web3.version);
- //判断用户是否安装MetaMask钱包插件
- if (typeof window.ethereum === "undefined") {
-    //没安装MetaMask钱包进行弹框提示
-    alert("Looks like you need a Dapp browser to get started.");
-    alert("Consider installing MetaMask!");
-} else {
-    //如果用户安装了MetaMask，你可以要求他们授权应用登录并获取其账号
-    ethereum.enable()
-    .catch(function (reason) {
-        //如果用户拒绝了登录请求
-        if (reason === "User rejected provider access") {
-            // 用户拒绝登录后执行语句；
-        } else {
-            // 本不该执行到这里，但是真到这里了，说明发生了意外
-            alert("There was an issue signing you in.");
-        }
-        }).then(function (accounts) {
-            //如果用户同意了登录请求，你就可以拿到用户的账号
-            var currentProvider = web3.currentProvider;
-            // var Web3 = web3.getWeb3();
-            // web3 = new Web3();
-            web3.setProvider(currentProvider);
-            contract = new web3.eth.Contract(contractAbi, contractAddress);
-            // console.log('地址列表', accounts)
-            //这里返回用户钱包地址
-            callback(accounts[0]);
-        });
-}
+export function init (callback){
+    if(window.ethereum){
+        window.ethereum.request({method:'eth_requestAccounts'}).then((res)=>{
+            console.log(res);
+            return callback(res[0])
+        })
+    }else{
+        alert('请安装metaMask钱包');
+    }
 };
 // 钱包签名
-function  sign(dataToSign,address,callback) {
+export function  sign(dataToSign,address,callback) {
     web3.eth.personal.sign(dataToSign.toString(), address, null).then(res=>{
         console.log(res);
            return callback(res)
@@ -48,7 +27,7 @@ function  sign(dataToSign,address,callback) {
  
 };
 // 转账
-function sendTransfer(address,values,callback,errCallBack){
+export function sendTransfer(address,values,callback,errCallBack){
     /*
         value:转账数量，
         from：转账地址
@@ -78,10 +57,4 @@ function sendTransfer(address,values,callback,errCallBack){
             console.log(res);
         }
     })
-} 
-
-export default{
-    init,
-    sign,
-    sendTransfer
-}
+};
